9b0c8f0b24c2706e5a4aba3f111ef6aba27453b3
Yazan Alqadery <yazanalqadery@gmail.com>
2023-09-28 02:12:09 +0300

    functional, responsive

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ holdBtn.addEventListener("click", () => {
     document.querySelector(`#score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add("player--winner");
